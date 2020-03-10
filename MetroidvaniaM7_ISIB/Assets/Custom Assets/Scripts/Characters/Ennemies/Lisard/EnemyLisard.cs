924026f33dfca26a8de396a8c00b5b76409a3f31
AllIntoine <45205288+AllIntoine@users.noreply.github.com>
2020-03-10 12:37:37 +0100

    enemy script up

---
+++ MetroidvaniaM7_ISIB/Assets/Custom Assets/Scripts/Characters/Ennemies/Lisard/EnemyLisard.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLisard : CharacterBehavior
+{
+    public Transform player;//PlayerBehavior player;
+    public float RangeofLangue;
+    public int dommage;
+    public int intervalAttacks;
+    public GameObject langue;
+
+    void Start()
+    {
+
+        MaxHp = 5;
+        Hp = MaxHp;
+        MoveSpeed = 10.0f;
+        JumpPower = 0.0f;
+        RangeofLangue = 5f;
+        dommage = 1;
+        intervalAttacks = 1000; 
+    }
+
+    void Update()
+    {
+    }
+
+    public void MoveToPlayer()
+    {
+        transform.LookAt(player.position);
+        transform.Rotate(new Vector3(0, -90, 0), Space.Self);
+        if (Vector3.Distance(transform.position, player.transform.position) > RangeofLangue)
+        {
+         transform.Translate(new Vector3(MoveSpeed * Time.deltaTime, 0, 0));        
+        }
+        else
+        {
+            System.Threading.Thread.Sleep(intervalAttacks);
+            langue.transform.localScale += new Vector3(RangeofLangue, 0, 0);      
+        }
+    }
+
+
+    public void test()
+        {
+        if (langue.transform.localScale.x > RangeofLangue)
+        {
+            System.Threading.Thread.Sleep(200);
+            langue.transform.localScale -= new Vector3(RangeofLangue, 0, 0);
+        }
+        }
+
+    public void TakeAHit()
+    {
+        Hp = Hp - 1;
+        if (Hp == 0)
+        {
+            // annimation mort 
+            Destroy(gameObject);
+        }
+    }
+    private void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("Toucher");
+        if (collider.tag == "Player")
+        {   
+            Debug.Log("player doit prendre dégat");
+        }
+    }
+}
---
+++ MetroidvaniaM7_ISIB/Assets/Custom Assets/Scripts/Characters/Ennemies/Spider/SpiderSpawner.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderSpawner : MonoBehaviour
+{
+    public BoxCollider territory;
+    GameObject player;
+    bool playerInTerritory;
+
+    public GameObject enemyx;
+    EnemySpider enemy;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        enemy = enemyx.GetComponent<EnemySpider>();
+        playerInTerritory = false;
+    }
+
+    void Update()
+    {
+        if (playerInTerritory == true)
+        {
+            enemy.MoveToPlayer();
+        }
+
+        if (playerInTerritory == false)
+        {
+            //rien faire
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            playerInTerritory = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            playerInTerritory = false;
+        }
+    }
+}
